3286408d8fa71cee4999bba4880efc18bcf02fa4
Christian Schwenger <drikdrok02@gmail.com>
2024-05-30 17:35:37 -0700

    Midi support

--- Theremin.cpp
+++ Theremin.cpp
@@ -11,6 +11,7 @@
 #include "al/app/al_App.hpp"
 #include "al/graphics/al_Font.hpp"
 #include "al/graphics/al_Shapes.hpp"
+#include "al/io/al_MIDI.hpp"
 #include "al/scene/al_PolySynth.hpp"
 #include "al/scene/al_SynthSequencer.hpp"
 #include "al/ui/al_ControlGUI.hpp"
@@ -43,6 +44,10 @@ struct NotePair {
         freq = f;
     }
 };
+
+float lerp(float a, float b, float t) {
+    return a + (b - a) * t;
+}
 
 class Theremin : public SynthVoice {
    public:
@@ -94,7 +99,10 @@ class Theremin : public SynthVoice {
         // will only be stored and aplied when a note is triggered.)
 
         createInternalTriggerParameter("amplitude", 0.3, 0.0, 1.0);
+        createInternalTriggerParameter("baseAmplitude", 0.3, 0.0, 1.0);
+
         createInternalTriggerParameter("frequency", 60, 20, 5000);
+        createInternalTriggerParameter("targetFrequency", 60, 20, 5000);
         createInternalTriggerParameter("attackTime", 0.01, 0.01, 3.0);
         createInternalTriggerParameter("releaseTime", 0.1, 0.1, 10.0);
         createInternalTriggerParameter("pan", 0.0, -1.0, 1.0);
@@ -118,6 +126,7 @@ class Theremin : public SynthVoice {
         // voice to hear the changes. Parameters will update values once per
         // audio callback because they are outside the sample processing loop.
         float oscFreq = getInternalParameterValue("frequency");
+
         float vibDepth = getInternalParameterValue("vibDepth");
         mAmpEnv.lengths()[0] = getInternalParameterValue("attackTime");
         mAmpEnv.lengths()[2] = getInternalParameterValue("releaseTime");
@@ -179,6 +188,78 @@ class Theremin : public SynthVoice {
     }
 };
 
+struct CallbackData {
+    Theremin *instrument;
+    bool *mousePlay;
+    float* timeSinceLastNote;
+};
+
+void midiCallback(double deltaTime, std::vector<unsigned char> *msg, void *userData) {
+    unsigned numBytes = msg->size();
+
+    if (numBytes > 0) {
+        // The first byte is the status byte indicating the message type
+        unsigned char status = msg->at(0);
+
+        CallbackData *data = static_cast<CallbackData *>(userData);
+        Theremin *instrument = data->instrument;
+        bool *mousePlay = data->mousePlay;
+        float* timeSinceLastNote = data->timeSinceLastNote;
+
+        printf("%s: ", MIDIByte::messageTypeString(status));
+
+        // Check if we received a channel message
+        if (MIDIByte::isChannelMessage(status)) {
+            unsigned char type = status & MIDIByte::MESSAGE_MASK;
+            unsigned char chan = status & MIDIByte::CHANNEL_MASK;
+
+            // Here we demonstrate how to parse to common channel messages
+            switch (type) {
+                case MIDIByte::NOTE_ON:
+                    // printf("Note %u, Vel %u \n", msg->at(1), msg->at(2));
+                    *mousePlay = false;
+                    if (*timeSinceLastNote > 0.6f){
+                        *timeSinceLastNote = 0;
+                    }
+
+                    instrument->setInternalParameterValue("targetFrequency", ::pow(2.f, ((msg->at(1)) - 69.f) / 12.f) * 432.f);
+
+                    break;
+
+                case MIDIByte::NOTE_OFF:
+                    // printf("Note %u, Vel %u \n", msg->at(1), msg->at(2));
+
+                    break;
+
+                case MIDIByte::PITCH_BEND:
+                    // printf("Value %u",MIDIByte::convertPitchBend(msg->at(1), msg->at(2)));
+                    break;
+
+                // Control messages need to be parsed again...
+                case MIDIByte::CONTROL_CHANGE:
+                    // printf("%s ", MIDIByte::controlNumberString(msg->at(1)));
+                    switch (msg->at(1)) {
+                        case MIDIByte::MODULATION:
+                            printf("%u", msg->at(2));
+                            break;
+                    }
+                    break;
+                default:;
+            }
+
+            printf(" (MIDI chan %u)", chan + 1);
+        }
+
+        printf("\n");
+
+        printf("\tBytes = ");
+        for (unsigned i = 0; i < numBytes; ++i) {
+            printf("%3u ", (int)msg->at(i));
+        }
+        printf(", stamp = %g\n", deltaTime);
+    }
+}
+
 // We make an app.
 class MyApp : public App {
    public:
@@ -187,15 +268,45 @@ class MyApp : public App {
     FontRenderer fontRender;
     int fontSize = 16;
 
+    bool mousePlay = true;
+
+    float timeSinceLastNote = 0;
+
+    float timer = 0;
+
     Theremin *instrument;
 
     std::vector<NotePair> notes = {
-        NotePair("E", 415.3), NotePair("F", 440), NotePair("F#", 466.2), NotePair("G", 493.9), NotePair("G#", 523.3), NotePair("A", 554.4), NotePair("A#", 587.3), NotePair("B", 622.3), NotePair("C", 659.3), NotePair("C#", 698.5), NotePair("D", 739.9), NotePair("D#", 783.9), NotePair("E", 830.6), NotePair("F", 880), NotePair("F#", 932.3), NotePair("G", 987.8), NotePair("G#", 1046.5), NotePair("A", 1108.7), NotePair("A#", 1174.7), NotePair("B", 1244.5), NotePair("C", 1318.5), NotePair("C#", 1396.9), NotePair("D", 1479.9), NotePair("D#", 1567.9), NotePair("E", 1661.2)
-    };
+        NotePair("G", 391.995),
+        NotePair("G#", 415.305),
+        NotePair("A", 440),
+        NotePair("A#", 466.164),
+        NotePair("B", 493.883),
+        NotePair("C", 523.251),
+        NotePair("C#", 554.365),
+        NotePair("D", 587.330),
+        NotePair("D#", 622.254),
+        NotePair("E", 659.255),
+        NotePair("F", 698.456),
+        NotePair("F#", 739.989),
+        NotePair("G", 783.991),
+        NotePair("G#", 830.609),
+        NotePair("A", 880),
+        NotePair("A#", 932.328),
+        NotePair("B", 987.767),
+        NotePair("C", 1046.5),
+        NotePair("C#", 1108.73),
+        NotePair("D", 1173.66),
+        NotePair("D#", 1244.61),
+        NotePair("E", 1318.51),
+        NotePair("F", 1396.91),
+        NotePair("F#", 1497.98),
+        NotePair("G", 1567.98),
+        NotePair("G#", 1661.22)};
 
+    RtMidiIn RtMidiIn;
 
-    void
-    onCreate() override {
+    void onCreate() override {
         navControl().active(
             false);  // Disable navigation via keyboard, since we
                      // will be using keyboard for note triggering
@@ -214,25 +325,92 @@ class MyApp : public App {
 
         // Play example sequence. Comment this line to start from scratch
         synthManager.synthRecorder().verbose(true);
+
+        // Check available ports vs. specified
+        unsigned portToOpen = 0;
+        unsigned numPorts = RtMidiIn.getPortCount();
+
+        if (portToOpen >= numPorts) {
+            printf("Invalid port specifier!\n");
+        }
+
+        try {
+            // Print out names of available input ports
+            for (unsigned i = 0; i < numPorts; ++i) {
+                printf("Port %u: %s\n", i, RtMidiIn.getPortName(i).c_str());
+            }
+
+            // Open the port specified above
+            RtMidiIn.openPort(portToOpen);
+        } catch (RtMidiError &error) {
+            error.printMessage();
+            // exit(1);
+        }
+
+        // Set our callback function.  This should be done immediately after
+        // opening the port to avoid having incoming messages written to the
+        // queue instead of sent to the callback function.
+        CallbackData *cbd = new CallbackData();
+        cbd->instrument = instrument;
+        cbd->mousePlay = &mousePlay;
+        cbd->timeSinceLastNote = &timeSinceLastNote;
+        RtMidiIn.setCallback(&midiCallback, cbd);  //&synthManager
+
+        // Don't ignore sysex, timing, or active sensing messages.
+        RtMidiIn.ignoreTypes(false, false, false);
     }
 
     // The audio callback function. Called when audio hardware requires data
-    void
-    onSound(AudioIOData &io) override {
+    void onSound(AudioIOData &io) override {
         synthManager.render(io);  // Render audio
     }
 
-    void
-    onAnimate(double dt) override {
+    void onAnimate(double dt) override {
         // The GUI is prepared here
         imguiBeginFrame();
         // Draw a window that contains the synth control panel
         synthManager.drawSynthControlPanel();
         imguiEndFrame();
+
+        timer += dt;
+        timeSinceLastNote += dt;
+
+        if (!mousePlay) {
+            float newFreq = instrument->getInternalParameter("frequency");
+            float targetFreq = instrument->getInternalParameter("targetFrequency");
+
+            float newAmp = instrument->getInternalParameter("baseAmplitude");
+            float currentAmp = instrument->getInternalParameter("amplitude");
+
+            if (abs(newFreq - targetFreq) > 10) {
+            }
+
+
+            float attack = 0;
+
+            if (timeSinceLastNote <= 0.4f){
+                attack = sinf(timeSinceLastNote * 3) * 0.3f;
+                newFreq = lerp(newFreq, targetFreq, dt * 4);
+            }else if (timeSinceLastNote <= 1.0f){
+                newFreq = lerp(newFreq, targetFreq, dt * 4);
+                newAmp = lerp(currentAmp, newAmp, dt * 4);
+            } else{
+                newAmp = lerp(currentAmp, newAmp, dt * 4);
+            }
+
+            newAmp += attack;
+
+
+            
+            //newFreq += rand() % 10 - 4.5f;
+            newFreq += sinf(timer * 40) * 600 * dt;
+
+            instrument->setInternalParameterValue("frequency", newFreq);
+            instrument->setInternalParameterValue("amplitude", newAmp);
+        }
     }
 
-    bool
-    onMouseMove(const Mouse &m) override {
+    bool onMouseMove(const Mouse &m) override {
         // Get the mouse position
         int x = m.x();
         int y = m.y();
@@ -241,16 +419,16 @@ class MyApp : public App {
 
         // std::cout << "pos: " << x << ", " << y << std::endl;
 
+        mousePlay = true;
         instrument->setInternalParameterValue("amplitude", clamp((float)(height() - (y + 50)) / (height() * 0.8f), 0, 1));
-
+        instrument->setInternalParameterValue("abseAmpltidue", instrument->getInternalParameter("amplitude"));
         // instrument->triggerOn();
 
         return true;
     }
 
     // The graphics callback function.
-    void
-    onDraw(Graphics &g) override {
+    void onDraw(Graphics &g) override {
         g.clear();
 
         // This example uses only the orthogonal projection for 2D drawing
@@ -264,6 +442,7 @@ class MyApp : public App {
         for (int i = 0; i < notes.size(); i++) {
             drawRect(g, notes[i].freq - 400, 70, 2, 40);
         }
+        drawRect(g, instrument->getInternalParameter("frequency") - 400, instrument->getInternalParameter("amplitude") * height() * 0.8 + 50, 4, 4);
 
         // For some reason rects won't draw after prints?
         for (int i = 0; i < notes.size(); i++) {
@@ -275,8 +454,7 @@ class MyApp : public App {
     }
 
     // Whenever a key is pressed, this function is called
-    bool
-    onKeyDown(Keyboard const &k) override {
+    bool onKeyDown(Keyboard const &k) override {
         if (ParameterGUI::usingKeyboard()) {  // Ignore keys if GUI is using
             // keyboard
             return true;
@@ -310,13 +488,11 @@ class MyApp : public App {
     onResize(int w, int h) override {
     }
 
-    void
-    onExit() override {
+    void onExit() override {
         imguiShutdown();
     }
 
-    void
-    drawRect(Graphics &g, int x, int y, int width, int height) {
+    void drawRect(Graphics &g, int x, int y, int width, int height) {
         g.tint(1, 1, 1);
         Mesh mesh;
         addRect(mesh, width, height, x + width / 2, y - height / 2);
